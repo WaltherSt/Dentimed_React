9c32fff84d217b03f058143a62b53e163ea8e3d2
Walter St <129338273+WaltherSt@users.noreply.github.com>
2024-01-22 18:39:13 -0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export default function App() {
   ];
   const titulo = "Implantes Dentales";
   const lista2 = ["Inicio", "Servicios", "Blog"];
-  const lista3 = ["Citas", "Pagos", "Agendar"];
+  const lista3 = ["Citas", "Pagos", "Agendar2"];
 
   const fechaActual = new Date(2024, 3, 1);
   return (
